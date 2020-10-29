a562ca5a48ecc57ed9a7909b0fca770789e70cdc
aparece1241 <apareceh7@gmail.com>
2020-10-29 14:58:43 +0800

    adds the product conf

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -4,21 +4,22 @@ const Router = express.Router();
 //user routes here
 const userController = require('../controllers/UserController');
 
-
+//authorization
+const AUTH = require('../middleware/DataAuthorization');
 
 //get all users
-Router.get("/all",userController.getUsers);
+Router.get("/all", AUTH.authorized, userController.getUsers);
 
 //update user
-Router.put("/update_user/:id",userController.updateUser);
+Router.put("/update_user/:id", AUTH.authorized, userController.updateUser);
 
 //delete user
-Router.delete("/delete/:id",UserController.deleteUser);
+Router.delete("/delete/:id", AUTH.authorized, UserController.deleteUser);
 
 //add user
-Router.post("/add",userController.addUser);
+Router.post("/add", AUTH.authorized, userController.addUser);
 
 //recieve user login credentials
-Router.post('/login',userController.login);
+Router.post('/login', userController.login);
 
 module.exports = Router;
